5a8d1c2e28f1b718df89a42f3fd8b8f09bd11636
crowcrow07 <skathd3402@gmail.com>
2023-11-30 19:23:09 +0900

    feat : MealItemForm 컴포넌트 제작

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,1 +1,21 @@
-export default function MealItemForm() {}
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+export default function MealItemForm({ id }) {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+}
