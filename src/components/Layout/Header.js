b9cc455b94efd7ffdb097a95501d22a0de4b506d
crowcrow07 <skathd3402@gmail.com>
2023-11-30 17:25:22 +0900

    feat : HeaderCartButton 컴포넌트

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,5 @@
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
-
 import classes from "./Header.module.css";
 
 export default function Header() {
@@ -7,7 +7,7 @@ export default function Header() {
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="mealsHeaderImage" />
